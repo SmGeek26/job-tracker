ed5e64e201e5dd5b2b2e6210a58e1d87f0413541
Sohini Mukherjee <sohinim2602@gmail.com>
2026-03-24 16:51:25 +0530

    remove local cors config

--- src/main/java/com/sohini/jobtracker/controller/UserController.java
+++ src/main/java/com/sohini/jobtracker/controller/UserController.java
@@ -7,7 +7,7 @@ import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
